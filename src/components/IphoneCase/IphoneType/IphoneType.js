3f8adf6c7aeac8f4212cc015becd8df202533924
goseeya <149542@gmail.com>
2019-07-19 21:48:56 +0200

    Camera hole in iphone case added

--- src/components/IphoneCase/IphoneType/IphoneType.js
+++ src/components/IphoneCase/IphoneType/IphoneType.js
@@ -9,16 +9,16 @@ class IphoneType extends Component {
 
     switch (this.props.type) {
       case ('iPhone6'):
-        type = <div className={styles.Iphone6}></div>;
+        type = <div className={styles.Iphone6}><div className={styles.IphoneCameraHole}></div></div>;
         break;
       case ('iPhone7'):
-        type = <div className={styles.Iphone7}></div>;
+        type = <div className={styles.Iphone7}><div className={styles.IphoneCameraHole}></div></div>;
         break;
       case ('iPhone8'):
-        type = <div className={styles.Iphone8}></div>;
+        type = <div className={styles.Iphone8}><div className={styles.IphoneCameraHole}></div></div>;
         break;
       case ('iPhone9'):
-        type = <div className={styles.Iphone9}></div>;
+        type = <div className={styles.Iphone9}><div className={styles.IphoneCameraHole}></div></div>;
         break;
       default:
         type = null;
