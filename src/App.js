b64a9dcb0a794e88190cf47b24da5ca603cd4b34
goseeya <149542@gmail.com>
2019-08-11 13:27:59 +0200

    Add react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Route, Switch } from 'react-router-dom';
 
 import Layout from './hoc/Layout/Layout';
 import IphoneCaseCreator from './containers/IphoneCaseCreator/IphoneCaseCreator';
@@ -9,8 +10,10 @@ class App extends Component {
     return (
       <div>
         <Layout>
-          <IphoneCaseCreator />
-          <Checkout />
+          <Switch>
+            <Route path="/checkout" component={Checkout} />
+            <Route path="/" exact component={IphoneCaseCreator} />
+          </Switch>
         </Layout>
       </div>
     );
