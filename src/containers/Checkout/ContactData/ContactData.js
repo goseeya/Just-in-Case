14c874a56f5909db4c5407931caa5c5e3fc543d0
goseeya <149542@gmail.com>
2019-08-18 20:57:56 +0200

    Handle user input

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -64,7 +64,7 @@ class ContactData extends Component {
     loading: false
   }
 
-  orderHandler = (event) => {
+  orderHandler = (event, inputIdentifier) => {
     event.preventDefault();
     alert('You continue!');
     this.setState({ loading: true });
@@ -83,6 +83,18 @@ class ContactData extends Component {
     });
   }
 
+  inputChangedHandler = (event, inputIdentifier) => {
+    const updatedOrderForm = {
+      ...this.state.orderForm
+    };
+    const updatedFormElement = {
+      ...updatedOrderForm[inputIdentifier]
+    };
+    updatedFormElement.value = event.target.velue;
+    updatedOrderForm[inputIdentifier] = updatedFormElement;
+    this.setState({orderForm: updatedOrderForm});
+  }
+
   render () {
     const formElementsArray = [];
     for (let key in this.state.orderForm) {
@@ -98,7 +110,8 @@ class ContactData extends Component {
             key={formElement.id}
             elementType={formElement.config.elementType}
             elementConfig={formElement.config.elementConfig}
-            elementValue={formElement.config.value} />
+            elementValue={formElement.config.value}
+            changed={(event) => this.inputChangedHandler(event, formElement.id)} />
         ))}
         <Button btnType="Success" clicked={this.orderHandler}>ORDER</Button>
       </form>
