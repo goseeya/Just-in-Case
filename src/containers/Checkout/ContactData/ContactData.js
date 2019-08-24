e4895dd83b3bdba59ee9cb69049c9ebfc51fc353
goseeya <149542@gmail.com>
2019-08-24 11:03:03 +0200

    Style disabled button

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -86,11 +86,10 @@ class ContactData extends Component {
             ]
           },
           value: 'Fastest',
-          validation: {
-            required: false
-          },
+          valid: true
         }
     },
+    formIsValid: false,
     loading: false
   }
 
@@ -149,7 +148,12 @@ class ContactData extends Component {
     updatedFormElement.touched = true;
     updatedOrderForm[inputIdentifier] = updatedFormElement;
     console.log(updatedFormElement);
-    this.setState({orderForm: updatedOrderForm});
+
+    let formIsValid = true;
+    for (let inputIdentifier in updatedOrderForm) {
+      formIsValid = updatedOrderForm[inputIdentifier].valid && formIsValid;
+    }
+    this.setState({ orderForm: updatedOrderForm, formIsValid: formIsValid });
   }
 
   render () {
@@ -173,7 +177,7 @@ class ContactData extends Component {
             touched={formElement.config.touched}
             changed={(event) => this.inputChangedHandler(event, formElement.id)} />
         ))}
-        <Button btnType="Success">ORDER</Button>
+        <Button btnType="Success" disabled={!this.state.formIsValid}>ORDER</Button>
       </form>
     );
     if (this.state.loading) {
