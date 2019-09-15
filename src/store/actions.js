847cad85129decf58d501b05c4e07397887a8ba9
goseeya <149542@gmail.com>
2019-09-15 11:56:25 +0200

    Apply thunk

--- src/store/actions.js
+++ src/store/actions.js
@@ -1,8 +1,16 @@
 export const CHANGE_TYPE = 'CHANGE_TYPE';
 
-export const changeType = (iType) => {
+export const modifyType = ( iType ) => {
   return {
     type: 'CHANGE_TYPE',
     iPhoneType: iType
+  }
+}
+
+export const changeType = (iType) => {
+  return dispatch => {
+    setTimeout(() => {
+      dispatch(modifyType(iType));
+    }, 2000);
   }
 };
