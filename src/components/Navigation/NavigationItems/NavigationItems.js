d265c5ce0b6ebeea57951c3957e8b5eba4b2aa44
goseeya <149542@gmail.com>
2019-12-23 22:29:30 +0100

    Add auth form

--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -8,6 +8,7 @@ const navigationItems = () => (
   <ul className={styles.NavigationItems}>
     <NavigationItem link="/" exact>iPhone Case</NavigationItem>
     <NavigationItem link="/orders">Shopping bag</NavigationItem>
+    <NavigationItem link="/auth">Authenticate</NavigationItem>
   </ul>
 );
 
