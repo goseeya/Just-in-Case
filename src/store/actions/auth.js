e4294cf94df7c3ccd3abd21904eb6d3de1902926
goseeya <149542@gmail.com>
2019-12-27 20:24:40 +0100

    Fix logout bug

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -31,8 +31,9 @@ export const logout = () => {
 export const checkAuthTimeout = (expirationTime) => {
   return dispatch => {
     setTimeout(() => {
-      dispatch(logout())
-    }, expirationTime * 1000)
+      dispatch(logout());
+      console.log(expirationTime);
+    }, expirationTime * 1000) ;
   };
 }
 
@@ -52,7 +53,7 @@ export const auth = (email, password, isSignup) => {
       .then(response => {
         console.log(response);
         dispatch(authSuccess(response.data.idToken, response.data.localId));
-        dispatch(checkAuthTimeout(response.data.espiresIn));
+        dispatch(checkAuthTimeout(response.data.expiresIn));
       })
       .catch(err => {
         console.log(err.response.data.error);
