64251a7dab76dd3480414a6d53f448ecea77d4bc
goseeya <149542@gmail.com>
2020-01-13 23:26:54 +0100

    Add guards

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import { Route, Switch, withRouter } from 'react-router-dom';
+import { Route, Switch, withRouter, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 
 import Layout from './hoc/Layout/Layout';
@@ -12,20 +12,40 @@ import * as actions from './store/actions/index';
 
 const App = (props) => {
     useEffect(() => props.onTryAutoSignup(), []);
+    let routes = (
+      <Switch>
+        <Route path="/auth" component={Auth} />
+        <Route path="/" exact component={IphoneCaseCreator} />
+        <Redirect to="/" />
+      </Switch>
+    );
+
+    if (props.isAuthenticated) {
+      routes = (
+        <Switch>
+          <Route path="/checkout" component={Checkout} />
+          <Route path="/orders" component={Orders} />
+          <Route path="/logout" component={Logout} />
+          <Route path="/" exact component={IphoneCaseCreator} />
+          <Redirect to="/" />
+        </Switch>
+      )
+    }
     return (
       <div>
         <Layout>
-          <Switch>
-            <Route path="/checkout" component={Checkout} />
-            <Route path="/orders" component={Orders} />
-            <Route path="/auth" component={Auth} />
-            <Route path="/logout" component={Logout} />
-            <Route path="/" exact component={IphoneCaseCreator} />
-          </Switch>
+          {routes}
         </Layout>
       </div>
     );
 }
+
+const mapStateToProps = state => {
+  return {
+
+    isAuthenticated: state.auth.token !== null
+  };
+};
 
 const mapDispatchToProps = dispatch => {
   return {
