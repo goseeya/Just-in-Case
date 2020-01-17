b24e1711ac560a87d59f12ffe0399955b40faddc
goseeya <149542@gmail.com>
2020-01-17 18:46:48 +0100

    Add test with setProps

--- src/components/Navigation/NavigationItems/NavigationItems.test.js
+++ src/components/Navigation/NavigationItems/NavigationItems.test.js
@@ -9,9 +9,20 @@ import NavigationItem from './NavigationItem/NavigationItem';
 configure({adapter: new Adapter()});
 
 describe('<NavigationItems />', () => {
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = shallow(<NavigationItems />);
+  });
+
   it('should render two <NavigtionItem /> elements if not authenticated', () => {
-    const wrapper = shallow(<NavigationItems />);
     //find is enzyme utility function
     expect(wrapper.find(NavigationItem)).toHaveLength(2);
+  });
+  
+  it('should render three <NavigtionItem /> elements if authenticated', () => {
+    // wrapper = shallow(<NavigationItems isAuthenticated />);
+    wrapper.setProps({isAuthenticated: true});
+    expect(wrapper.find(NavigationItem)).toHaveLength(3);
   });
 });
