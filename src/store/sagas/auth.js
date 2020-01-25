fd4fb30c032d3f44461a2a5176801b610a6073ac
goseeya <149542@gmail.com>
2020-01-25 15:04:02 +0100

    Add saga file for auth

---
+++ src/store/sagas/auth.js
@@ -0,0 +1,13 @@
+import { put } from 'redux-saga/effects';
+
+import * as actionTypes from '../actions/actionTypes';
+
+function* logout(action) {
+  // yield means that these should be executed and we will wait for them to finish
+  yield localStorage.removeItem('token');
+  yield localStorage.removeItem('expirationTime');
+  yield localStorage.removeItem('localId');
+  yield put({
+    type: actionTypes.AUTH_LOGOUT
+  });
+}
