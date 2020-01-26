4c2383cf57fa80062009873348924de5941f4618
goseeya <149542@gmail.com>
2020-01-26 21:53:19 +0100

    Add delay to saga

--- src/store/sagas/auth.js
+++ src/store/sagas/auth.js
@@ -1,13 +1,18 @@
+import { delay } from 'redux-saga/effects';
 import { put } from 'redux-saga/effects';
 
 import * as actionTypes from '../actions/actionTypes';
+import * as actions from '../actions/index';
 
-export function* LogoutSaga(action) {
+export function* logoutSaga(action) {
   // yield means that these should be executed and we will wait for them to finish
   yield localStorage.removeItem('token');
   yield localStorage.removeItem('expirationTime');
   yield localStorage.removeItem('localId');
-  yield put({
-    type: actionTypes.AUTH_LOGOUT
-  });
+  yield put(actions.logoutSucceed());
+}
+
+export function* checkAuthTimeoutSaga(action) {
+  yield delay(action.expirationTime / 1000);
+  yield put(actions.logout());
 }
